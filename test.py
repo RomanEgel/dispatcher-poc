12e8e40e24cf80a19f874f70dcc62e9280733d4d
Roman Egel <reg-ext@marketlogicsoftware.com>
2022-10-13 10:35:13 +0400

    fix

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from gym.wrappers import FlattenObservation
 import gym
-import gym_examples.gym_examples
+import gym_examples
 
 env = gym.make('GridWorld-v0')
 wrapped_env = FlattenObservation(env)
