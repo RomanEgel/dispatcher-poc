924f473e41eda209d1acc4d80cb65dabcb95f638
Roman Egel <reg-ext@marketlogicsoftware.com>
2022-10-13 10:39:04 +0400

    fix 3

--- gym_examples/gym_examples/__init__.py
+++ gym_examples/gym_examples/__init__.py
@@ -2,6 +2,6 @@ from gym.envs.registration import register
 
 register(
     id='GridWorld-v0',
-    entry_point='gym_examples.envs:GridWorldEnv',
+    entry_point='gym_examples.gym_examples.envs:GridWorldEnv',
     max_episode_steps=300
 )
