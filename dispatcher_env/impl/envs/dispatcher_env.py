030c33e955b2b8167e690614f2a4432e191db4ef
Roman Egel <reg-ext@marketlogicsoftware.com>
2022-10-16 09:04:16 +0400

    some fixes

--- dispatcher_env/impl/envs/dispatcher_env.py
+++ dispatcher_env/impl/envs/dispatcher_env.py
@@ -2,38 +2,35 @@ import gym
 from gym import spaces
 import pygame
 import numpy as np
+import time
 
 
 class DispatcherEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
-    def __init__(self, render_mode=None, size=5):
-        self.size = size  # The size of the square grid
+    def __init__(self, render_mode=None, number_of_tenants=5):
+        self.tenants_number = number_of_tenants  # The number of tenants
         self.window_size = 512  # The size of the PyGame window
+        self.max_tasks_buffer = 1024
 
-        # Observations are dictionaries with the agent's and the target's location.
-        # Each location is encoded as an element of {0, ..., `size`}^2, i.e. MultiDiscrete([size, size]).
+        # Observation is a dictionary with tenant's tasks current information
+        # Current information consists of 3 dimensions, which is:
+        # number of tasks,
+        # moving average of processing time (MA_proc_time)
+        # moving average of number of tasks (MA_num_of_tasks)
         self.observation_space = spaces.Dict(
             {
-                "agent": spaces.Box(0, size - 1, shape=(2,), dtype=int),
-                "target": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "tasks_queue": spaces.Box(0, 100000, shape=[self.tenants_number], dtype=int),
+                "ma_tasks_duration": spaces.Box(0, 1000,  shape=[self.tenants_number], dtype=float),
+                "ma_tasks_number": spaces.Box(0, 1000, shape=[self.tenants_number], dtype=float),
             }
         )
 
-        # We have 4 actions, corresponding to "right", "up", "left", "down"
-        self.action_space = spaces.Discrete(4)
+        # We have 'number_of_tenants' + 1 actions, corresponding to picking task from corresponding tenant or Nothing (-1)
+        self.action_space = spaces.Discrete(number_of_tenants + 1, start=-1)
 
-        """
-        The following dictionary maps abstract actions from `self.action_space` to 
-        the direction we will walk in if that action is taken.
-        I.e. 0 corresponds to "right", 1 to "up" etc.
-        """
-        self._action_to_direction = {
-            0: np.array([1, 0]),
-            1: np.array([0, 1]),
-            2: np.array([-1, 0]),
-            3: np.array([0, -1]),
-        }
+        self.tasks_data = None
+        self.incoming_tasks = None
 
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
@@ -49,24 +46,22 @@ class DispatcherEnv(gym.Env):
         self.clock = None
 
     def _get_obs(self):
-        return {"agent": self._agent_location, "target": self._target_location}
+        return {"tasks_queue": self._tasks_queue, "ma_tasks_duration": self._ma_tasks_duration, "ma_tasks_number": self._ma_tasks_number}
 
     def _get_info(self):
-        return {"distance": np.linalg.norm(self._agent_location - self._target_location, ord=1)}
+        return {"tasks_data": self.tasks_data, "incoming_tasks": self.incoming_tasks}
 
     def reset(self, seed=None, options=None):
         # We need the following line to seed self.np_random
         super().reset(seed=seed)
 
-        # Choose the agent's location uniformly at random
-        self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
+        # zero tenants tasks
+        self._tasks_queue = np.zeros((self.tenants_number), dtype=int)
+        self._ma_tasks_duration = np.zeros((self.tenants_number))
+        self._ma_tasks_number = np.zeros((self.tenants_number))
+        self.tasks_data = [[] for _ in range(self.tenants_number)]
 
-        # We will sample the target's location randomly until it does not coincide with the agent's location
-        self._target_location = self._agent_location
-        while np.array_equal(self._target_location, self._agent_location):
-            self._target_location = self.np_random.integers(
-                0, self.size, size=2, dtype=int
-            )
+        self.incoming_tasks = self.np_random.integers(10000, 100000, size=self.tenants_number)
 
         observation = self._get_obs()
         info = self._get_info()
@@ -77,22 +72,62 @@ class DispatcherEnv(gym.Env):
         return observation, info
 
     def step(self, action):
-        # Map the action (element of {0,1,2,3}) to the direction we walk in
-        direction = self._action_to_direction[action]
-        # We use `np.clip` to make sure we don't leave the grid
-        self._agent_location = np.clip(
-            self._agent_location + direction, 0, self.size - 1
-        )
-        # An episode is done iff the agent has reached the target
-        terminated = np.array_equal(self._agent_location, self._target_location)
-        reward = 1 if terminated else 0  # Binary sparse rewards
+        if self.np_random.random() > 0.5:
+            picked_tenants = self.np_random.choice(self.tenants_number, size=self.np_random.integers(1, self.tenants_number / 2))
+            for tenant in picked_tenants:
+                if self.incoming_tasks[tenant] > 0:
+                    pick_tasks = self.np_random.integers(1, np.minimum(10, self.incoming_tasks[tenant]))
+                    self.incoming_tasks[tenant] -= pick_tasks
+                    self._tasks_queue[tenant] += pick_tasks
+                    for _ in range(pick_tasks):
+                        self.tasks_data[tenant].append(time.time())
+
+        reward = 0
+        total_tasks = np.sum(self._tasks_queue)
+
+        terminal = False
+        if action != -1 and (self._tasks_queue[action] == 0):
+            reward -= 100
+        else:
+            time_per_task = np.zeros(self.tenants_number)
+            for tenant in range(self.tenants_number):
+                if action == tenant:
+                    self._tasks_queue[tenant] -= 1
+                    self.tasks_data[tenant].pop(0)
+                    total_tasks -= 1
+                    reward += 1
+                tasks_num = self._tasks_queue[tenant]
+
+                if tasks_num < 1:
+                    tasks_duration = 0
+                    time_per_task[tenant] = 0
+                else:
+                    tasks_duration = self.tasks_data[tenant][tasks_num - 1] - self.tasks_data[tenant][0]
+                    time_per_task[tenant] = tasks_duration / tasks_num
+
+                    reward += np.maximum(-5.0, (self._ma_tasks_duration[tenant] - tasks_duration) / self.tenants_number)
+                    reward += np.maximum(-5.0, (self._ma_tasks_number[tenant] - tasks_num) / self.tenants_number)
+
+                self._ma_tasks_duration[tenant] = 0.9 * self._ma_tasks_duration[tenant] + 0.1 * (tasks_duration)
+                self._ma_tasks_number[tenant] = 0.9 * self._ma_tasks_number[tenant] + 0.1 * (tasks_num)
+
+            var = np.std(time_per_task)
+            if var > 1.0:
+                reward -= np.minimum(5.0, var)
+
+            if action != -1 and total_tasks == 0:
+                reward += 100
+
+        if np.sum(self.incoming_tasks) == 0 and total_tasks == 0:
+            terminal = True
+
         observation = self._get_obs()
         info = self._get_info()
 
         if self.render_mode == "human":
             self._render_frame()
 
-        return observation, reward, terminated, False, info
+        return observation, reward, terminal, False, info
 
     def render(self):
         if self.render_mode == "rgb_array":
@@ -108,41 +143,35 @@ class DispatcherEnv(gym.Env):
 
         canvas = pygame.Surface((self.window_size, self.window_size))
         canvas.fill((255, 255, 255))
-        pix_square_size = (
-                self.window_size / self.size
+
+        pix_tenant_border_size = (
+                self.window_size / self.tenants_number
         )  # The size of a single grid square in pixels
 
-        # First we draw the target
-        pygame.draw.rect(
-            canvas,
-            (255, 0, 0),
-            pygame.Rect(
-                pix_square_size * self._target_location,
-                (pix_square_size, pix_square_size),
-            ),
-        )
-        # Now we draw the agent
-        pygame.draw.circle(
-            canvas,
-            (0, 0, 255),
-            (self._agent_location + 0.5) * pix_square_size,
-            pix_square_size / 3,
-        )
-
-        # Finally, add some gridlines
-        for x in range(self.size + 1):
+        # First we draw each tenant borders
+        for x in range(self.tenants_number + 1):
             pygame.draw.line(
                 canvas,
                 0,
-                (0, pix_square_size * x),
-                (self.window_size, pix_square_size * x),
+                (pix_tenant_border_size * x, 0),
+                (pix_tenant_border_size * x, self.window_size),
                 width=3,
             )
+
+        # then we draw each tenant stack of tasks
+        max = np.max(self._tasks_queue)
+        color = 255 // self.tenants_number
+        for i in range(self.tenants_number):
+            if max == 0.:
+                dist = 0
+            else:
+                dist = self._tasks_queue[i] / max
+
             pygame.draw.line(
                 canvas,
-                0,
-                (pix_square_size * x, 0),
-                (pix_square_size * x, self.window_size),
+                (color * i, 0, 0),
+                (pix_tenant_border_size * i, self.window_size - (self.window_size - 40) * dist + 20),
+                (pix_tenant_border_size * (i + 1), self.window_size - (self.window_size - 40) * dist + 20),
                 width=3,
             )
 
--- test.py
+++ test.py
@@ -1,6 +1,14 @@
-from gym.wrappers import FlattenObservation
 import gym
 
-env = gym.make('dispatcher_env.impl:DispatcherEnv-v0')
-wrapped_env = FlattenObservation(env)
-print(wrapped_env.reset())     # E.g.  [3 0 3 3], {}
+env = gym.make('dispatcher_env.impl:DispatcherEnv-v0', render_mode="human")
+env.action_space.seed(42)
+observation, info = env.reset(seed=42)
+
+for _ in range(100):
+    observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+
+    if terminated or truncated:
+        observation, info = env.reset()
+    print(observation)
+
+env.close()
